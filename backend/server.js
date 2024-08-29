00baa9874148fb888b6b455479351fe35f3821ef
Mukul Rajput <148499007+190764@users.noreply.github.com>
2024-08-30 00:12:19 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRouter = require('./routes/userRoute');
 const passwordRouter = require('./routes/passwordRoute');
 const otpRouter = require('./routes/otpRoute');
 const cors = require('cors');
+const path = require("path");
 
 const server = express();
 (async function () {
@@ -19,4 +20,9 @@ server.use('/password-api/passwords', passwordRouter);
 server.use('/password-api/otp', otpRouter);
 server.use(errorHandler);
 
+server.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+
+server.all("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+});
 server.listen(3000, () => console.log('Server started'));
